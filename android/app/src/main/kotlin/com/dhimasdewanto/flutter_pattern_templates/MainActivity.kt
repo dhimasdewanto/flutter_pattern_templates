346cb853f8b9ce71c86dcb488872418e6a9cc19a
Dhimas Dewanto <dhimasbrd@gmail.com>
2020-07-25 07:58:47 +0700

    Refactor Android

--- android/app/src/main/kotlin/com/dhimasdewanto/flutter_pattern_templates/MainActivity.kt
+++ android/app/src/main/kotlin/com/dhimasdewanto/flutter_pattern_templates/MainActivity.kt
@@ -1,63 +1,18 @@
 package com.dhimasdewanto.flutter_pattern_templates
 
-import android.content.Context
-import android.content.Intent
-import android.os.BatteryManager
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
-import io.flutter.plugin.common.MethodChannel
 
 class MainActivity : FlutterActivity() {
-    companion object {
-        private const val BATTERY_CHANNEL = "com.dhimasdewanto.flutter_pattern_templates/battery"
-        private const val BATTERY_METHOD = "get_battery_level"
 
-        private const val ACTIVITY_CHANNEL = "com.dhimasdewanto.flutter_pattern_templates/activity"
-        private const val ACTIVITY_METHOD = "new_activity"
-    }
 
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
 
-        callBatteryChannel()
-        callNewActivityChannel()
-    }
+        val batteryChannel = BatteryChannel(flutterEngine, this)
+        val newActivityChannel = NewActivityChannel(flutterEngine, this)
 
-    private fun callNewActivityChannel() {
-        MethodChannel(flutterEngine?.dartExecutor?.binaryMessenger, ACTIVITY_CHANNEL).setMethodCallHandler { call, result ->
-
-            if (call.method == ACTIVITY_METHOD) {
-                val intent = Intent(this, SecondActivity::class.java)
-                startActivity(intent)
-                result.success(true)
-                return@setMethodCallHandler
-            }
-
-            result.notImplemented()
-        }
-    }
-
-    private fun callBatteryChannel() {
-        MethodChannel(flutterEngine?.dartExecutor?.binaryMessenger, BATTERY_CHANNEL).setMethodCallHandler { call, result ->
-
-            if (call.method == BATTERY_METHOD) {
-                val batteryLevel = getBatteryLevel()
-
-                if (batteryLevel == -1) {
-                    result.error("UNAVAILABLE", "Battery level not available.", null)
-                    return@setMethodCallHandler
-                }
-
-                result.success(batteryLevel)
-                return@setMethodCallHandler
-            }
-
-            result.notImplemented()
-        }
-    }
-
-    private fun getBatteryLevel(): Int {
-        val batteryManager = getSystemService(Context.BATTERY_SERVICE) as BatteryManager
-        return batteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY)
+        batteryChannel.callChannel()
+        newActivityChannel.callChannel()
     }
 }
