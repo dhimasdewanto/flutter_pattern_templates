2b802eddd1105f69c1c1348e7babd56d60ec1360
Dhimas Dewanto <dhimasbrd@gmail.com>
2022-09-21 16:38:52 +0700

    Setup env

---
+++ lib/core/configs/env.dart
@@ -0,0 +1,6 @@
+typedef EnvString = String;
+
+abstract class Env {
+  static const EnvString prod = "prod";
+  static const EnvString dev = "dev";
+}
---
+++ lib/main_prod.dart
@@ -0,0 +1,6 @@
+import 'core/configs/env.dart';
+import 'main_app.dart';
+
+void main() {
+  mainApp(Env.prod);
+}
